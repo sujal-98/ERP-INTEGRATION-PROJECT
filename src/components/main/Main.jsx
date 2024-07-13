827863b0b6be94a012033d894ae536ceb0f66910
DHRUV GABA <dhruv18gaba@gmail.com>
2024-07-14 01:39:32 +0530

    added pagination, modified sidebar and speed dial

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import Card from "./card";
 import "./Main.css";
 
 const Main = ({ students }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const cardsPerPage = 20;
+
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = students.slice(indexOfFirstCard, indexOfLastCard);
+  const totalPages = Math.ceil(students.length / cardsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
+    <>
     <div className="cards-container">
-      {students.length > 0 ? (
-        students.map((student, index) => (
+      {currentCards.length > 0 ? (
+        currentCards.map((student, index) => (
           <Card key={index} data={student} />
         ))
       ) : (
@@ -18,13 +31,35 @@ const Main = ({ students }) => {
           <div id="text">Nothing to do ...</div>
         </div>
       )}
-    </div>
+      </div>
+      {/* Pagination Controls */}
+      {totalPages > 1 && (
+        <div className="pagination">
+          {currentPage > 1 && (
+            <button onClick={() => handlePageChange(1)}>{'<<'}</button>
+          )}
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              className={currentPage === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </button>
+          ))}
+          {currentPage < totalPages && (
+            <button onClick={() => handlePageChange(totalPages)}>{'>>'}</button>
+          )}
+        </div>
+      )}
+
+    </>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    students: state.students, 
+    students: state.students,
   };
 };
 
