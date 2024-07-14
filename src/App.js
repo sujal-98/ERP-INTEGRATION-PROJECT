c8f79c17173e23042fe9076e223891b099f40bc3
DHRUV GABA <dhruv18gaba@gmail.com>
2024-07-14 22:55:53 +0530

    pdf Preview, cards modification

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import PdfPreview from './pdf/PdfPreview';
+import React from 'react';
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
       <Router>
         <Routes>
       <Route path='/' element={<Home />} />
+      <Route path="/pdf-preview" element={<PdfPreview />} />
       </Routes>
       </Router>
     </div>
--- src/components/main/card.jsx
+++ src/components/main/card.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Main.css';
-import {generatePdf} from '../../pdf/renderPdf';
-
-
+import { generatePdfUrl, downloadPdf } from '../../pdf/renderPdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faDownload } from '@fortawesome/free-solid-svg-icons'; // Import icons you need
 
 
 const Card = ({ data }) => {
-  const handleClick=(data)=>{
-    generatePdf(data)
-  }
+  const navigate = useNavigate();
+
+  const handlePreviewClick = async (student) => {
+    const pdfUrl = await generatePdfUrl(student);
+    console.log(pdfUrl); // Log the URL to check if it's valid
+    navigate('/pdf-preview', { state: { pdfUrl } });
+  };
+  
+
+  const handleDownloadClick = async (student) => {
+    await downloadPdf(student);
+  };
+
   return (
-    <div className='card' onClick={()=>{handleClick(data.student)}}>
+    <div className='card'>
       <img src="/assets/photo1.jpg" alt={`${data.student.name}`} />
       <p>
         <b>{data.student.name}</b><br />
         {data.student.rollno}<br />
         {data.student.year} ({data.student.branch})
       </p>
+      <button onClick={() => handlePreviewClick(data.student)} style={{position:"absolute"}}><FontAwesomeIcon icon={faEye} style={{color:"rgb(100, 169, 249)", fontSize:"1.5rem"}} /></button>
+      <button onClick={() => handleDownloadClick(data.student)} style={{position:"relative", bottom:"8px"}}><FontAwesomeIcon icon={faDownload} style={{color:"rgb(100, 169, 249)", fontSize:"1.5rem"}}/></button>
     </div>
   );
 };
